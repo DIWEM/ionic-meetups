c76226a00a563239b67a4700c949d2d5796d9185
Mauricio G <mau.gar.gonz@icloud.com>
2019-06-29 20:29:30 -0500

    update

--- Framework/src/app/tabs/tab1/tab1.page.ts
+++ Framework/src/app/tabs/tab1/tab1.page.ts
@@ -11,9 +11,47 @@ import { Router, ActivatedRoute } from '@angular/router';
 })
 export class Tab1Page implements OnInit {
 
-  value = "hola";
+  qrData = "0"; //Para integrar al createdCode
+  createdCode = null; //Crea el QR
+
+  //Variables JSON QR
+  jsonQRot = "0001";
+  jsonQRalias = "Alias";
+  jsonQRcl = "012180004585820842";
+  jsonQRtype = "CL";
+  jsonQRrefn = "0";
+  jsonQRrefa = "MAURICIO";
+  jsonQRamount = "0";
+  jsonQRbank = "00012";
+  jsonQRcountry = "MX";
+  jsonQRcurrency = "MXN";
+  //Fin de Variable JSON QR
+
+
+  //JSON QR
+  obj = {
+    "ot": this.jsonQRot,
+    "dOp":[
+      {"alias": this.jsonQRalias},
+      {"cl": this.jsonQRcl},
+      {"type": this.jsonQRtype},
+      {"refn": this.jsonQRrefn},
+      {"refa": this.jsonQRrefa},
+      {"amount": this.qrData},
+      {"bank": this.jsonQRbank},
+      {"country": this.jsonQRcountry},
+      {"currency": this.jsonQRcurrency}
+    ]
+  };
+  qrDataJson = JSON.stringify(this.obj);
+  //Fin de JSON QR
 
   items: Array<any>;
+
+  qrURL = this.qrDataJson;
+  User = "000001/";
+
+  qrCode = this.qrURL.concat(this.User)
 
   constructor(
     public loadingCtrl: LoadingController,
@@ -26,6 +64,10 @@ export class Tab1Page implements OnInit {
     if (this.route && this.route.data) {
       this.getData();
     }
+  }
+
+  createCode() {
+    this.createdCode = this.qrData;
   }
 
   async getData(){
@@ -66,5 +108,212 @@ export class Tab1Page implements OnInit {
       event.target.complete();
     }, 2000);
   }
+
+
+
+
+
+
+
+
+
+
+
+    //Control de Págo
+    oneButton(){
+      if (this.qrData.length > 4){
+        this.createdCode = this.qrData.slice(0,5);
+        this.qrCode = this.myJSON.concat(this.createdCode)//Crea un QR Completo
+      }
+      else{
+        if (this.qrData == "0"){
+          this.qrData = "1"
+          this.createdCode = this.qrData;
+          this.qrCode = this.qrURL.concat(this.createdCode) //Crea un QR Completo
+        }
+        else{
+          this.qrData = this.qrData + "1"
+          this.createdCode = this.qrData;
+          this.qrCode = this.qrURL.concat(this.createdCode) //Crea un QR Completo
+        }
+      }
+      }
+    
+      twoButton(){
+      if (this.qrData.length > 4){
+        this.createdCode = this.qrData.slice(0,5);
+        this.qrCode = this.qrURL.concat(this.createdCode) //Crea un QR Completo
+      }
+      else{
+        if (this.qrData == "0"){
+          this.qrData = "2"
+          this.createdCode = this.qrData;
+          this.qrCode = this.qrURL.concat(this.createdCode) //Crea un QR Completo
+        }
+        else{
+          this.qrData = this.qrData + "2"
+          this.createdCode = this.qrData;
+          this.qrCode = this.qrURL.concat(this.createdCode) //Crea un QR Completo
+        }
+      }
+      }
+    
+      threeButton(){
+      if (this.qrData.length > 4){
+        this.createdCode = this.qrData.slice(0,5);
+        this.qrCode = this.qrURL.concat(this.createdCode) //Crea un QR Completo
+      }
+      else{
+        if (this.qrData == "0"){
+          this.qrData = "3"
+          this.createdCode = this.qrData;
+          this.qrCode = this.qrURL.concat(this.createdCode) //Crea un QR Completo
+        }
+        else{
+          this.qrData = this.qrData + "3"
+          this.createdCode = this.qrData;
+          this.qrCode = this.qrURL.concat(this.createdCode) //Crea un QR Completo
+        }
+      }
+      }
+    
+      fourButton(){
+      if (this.qrData.length > 4){
+        this.createdCode = this.qrData.slice(0,5);
+        this.qrCode = this.qrURL.concat(this.createdCode) //Crea un QR Completo
+      }
+      else{
+        if (this.qrData == "0"){
+          this.qrData = "4"
+          this.createdCode = this.qrData;
+          this.qrCode = this.qrURL.concat(this.createdCode) //Crea un QR Completo
+        }
+        else{
+          this.qrData = this.qrData + "4"
+          this.createdCode = this.qrData;
+          this.qrCode = this.qrURL.concat(this.createdCode) //Crea un QR Completo
+        }
+      }
+      }
+    
+      fiveButton(){
+      if (this.qrData.length > 4){
+        this.createdCode = this.qrData.slice(0,5);
+        this.qrCode = this.qrURL.concat(this.createdCode) //Crea un QR Completo
+      }
+      else{
+        if (this.qrData == "0"){
+          this.qrData = "5"
+          this.createdCode = this.qrData;
+          this.qrCode = this.qrURL.concat(this.createdCode) //Crea un QR Completo
+        }
+        else{
+          this.qrData = this.qrData + "5"
+          this.createdCode = this.qrData;
+          this.qrCode = this.qrURL.concat(this.createdCode) //Crea un QR Completo
+        }
+      }
+      }
+    
+      sixButton(){
+      if (this.qrData.length > 4){
+        this.createdCode = this.qrData.slice(0,5);
+        this.qrCode = this.qrURL.concat(this.createdCode) //Crea un QR Completo
+      }
+      else{
+        if (this.qrData == "0"){
+          this.qrData = "6"
+          this.createdCode = this.qrData;
+          this.qrCode = this.qrURL.concat(this.createdCode) //Crea un QR Completo
+        }
+        else{
+          this.qrData = this.qrData + "6"
+          this.createdCode = this.qrData;
+          this.qrCode = this.qrURL.concat(this.createdCode) //Crea un QR Completo
+        }
+      }
+      }
+    
+      sevenButton(){
+      if (this.qrData.length > 4){
+        this.createdCode = this.qrData.slice(0,5);
+        this.qrCode = this.qrURL.concat(this.createdCode) //Crea un QR Completo
+      }
+      else{
+        if (this.qrData == "0"){
+          this.qrData = "7"
+          this.createdCode = this.qrData;
+          this.qrCode = this.qrURL.concat(this.createdCode) //Crea un QR Completo
+        }
+        else{
+          this.qrData = this.qrData + "7"
+          this.createdCode = this.qrData;
+          this.qrCode = this.qrURL.concat(this.createdCode) //Crea un QR Completo
+        }
+      }
+      }
+    
+      eightButton(){
+      if (this.qrData.length > 4){
+        this.createdCode = this.qrData.slice(0,5);
+        this.qrCode = this.qrURL.concat(this.createdCode) //Crea un QR Completo
+      }
+      else{
+        if (this.qrData == "0"){
+          this.qrData = "8"
+          this.createdCode = this.qrData;
+          this.qrCode = this.qrURL.concat(this.createdCode) //Crea un QR Completo
+        }
+        else{
+          this.qrData = this.qrData + "8"
+          this.createdCode = this.qrData;
+          this.qrCode = this.qrURL.concat(this.createdCode) //Crea un QR Completo
+        }
+      }
+      }
+    
+      nineButton(){
+      if (this.qrData.length > 4){
+        this.createdCode = this.qrData.slice(0,5);
+        this.qrCode = this.qrURL.concat(this.createdCode) //Crea un QR Completo
+      }
+      else{
+        if (this.qrData == "0"){
+          this.qrData = "9"
+          this.createdCode = this.qrData;
+          this.qrCode = this.qrURL.concat(this.createdCode) //Crea un QR Completo
+        }
+        else{
+          this.qrData = this.qrData + "9"
+          this.createdCode = this.qrData;
+          this.qrCode = this.qrURL.concat(this.createdCode) //Crea un QR Completo
+        }
+      }
+      }
+    
+      zeroButton(){
+      if (this.qrData.length > 4){
+        this.createdCode = this.qrData.slice(0,5);
+        this.qrCode = this.qrURL.concat(this.createdCode) //Crea un QR Completo
+      }
+      else{
+        if (this.qrData == "0"){
+          this.qrData = "0"
+          this.createdCode = this.qrData;
+          this.qrCode = this.qrURL.concat(this.createdCode) //Crea un QR Completo
+        }
+        else{
+          this.qrData = this.qrData + "0"
+          this.createdCode = this.qrData;
+          this.qrCode = this.qrURL.concat(this.createdCode) //Crea un QR Completo
+        }
+      }
+      }
+    
+      cleanButton(){
+        this.qrData = "0"
+        this.createdCode = this.qrData;
+        this.qrCode = this.qrURL.concat(this.createdCode) //Crea un QR Completo
+      }
 
 }
